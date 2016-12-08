01998d8323b71b00aad0753a5272ddb7f386d60e
dengjiao <dengjiao1121@163.com>
2016-12-08 14:12:15 +0800

    聊天功能基本实现，在将消息发送给其他的用户时接收不到

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server); //引入socket.io模块并绑定到服务器
-    user = [];
+    users = [];  //保存所有在线用户昵称
 app.use('/', express.static(__dirname + '/www'));
 server.listen(80);
  
 //socket部分
 io.on('connection', function(socket) {
     //昵称设置
-    socket.on('foo', function(nickname) {
+    socket.on('login', function(nickname) {
        if (users.indexOf(nickname)>-1){
        	socket.emit('nickExisted');
        }else{
@@ -21,5 +21,16 @@ io.on('connection', function(socket) {
        	io.sockets.emit('system',nickname,users.length,'login');
        };
     });
+    socket.on('disconnect',function(){
+    	//将断开连接的用户从用户中删除
+    	users.splice(socket.userIndex,1);
+		//通知除自己以外的所有人
+		socket.broadcast.emit('system',socket.nickname,users.length,'logout'); 
+    });  
+
+    socket.on('postMsg',function(msg){
+    	//将消息传送到出自己以外的所有用户
+    	socket.broadcast.emit('newMsg',socket.nickname,msg);
+    });     
 });
 
--- www/scripts/HiChat.js
+++ www/scripts/HiChat.js
@@ -34,16 +34,57 @@ HiChat.prototype = {
 				document.getElementById('nicknameInput').focus();
 			};
 		},false);
+
 		this.socket.on('nickExisted',function(){
 			document.getElementById('info').textContent = 'nickName is taken,choose another pls';
 		});//显示昵称倍占用的提示
+
 		this.socket.on('loginSuccess',function(){
 			document.title = 'hichat|'+ document.getElementById('nicknameInput').value;
-			document.getElementById('loginWrapper').style.display = "none"
+			document.getElementById('loginWrapper').style.display = "none";
 			//隐藏遮罩层显示聊天
 			document.getElementById('messageInput').focus();
 			//让消息输入并且获得焦点
 		});
+
+		//显示在线人数及人员进出
+		this.socket.on('system',function(nickName,userCount,type){
+			var msg = nickName + (type == 'login'?'joined':'left');
+			// var p = document.createElement('p');
+			// p.textContent = msg;
+			// document.getElementById('historyMsg').appendChild(p);
+			//指定系统中的颜色为红色
+			that._displayNewMsg('system',msg,'red');
+			//将在线人数显示在页面顶部
+			document.getElementById('status').textContent = userCount + (userCount>1?"users":"user") + 'online';
+		});
+
+		this.socket.on('newMsg',function(user,msg){
+			that.
+			(user,msg);
+		});
+
+		document.getElementById('sendBtn').addEventListener('click',function(){
+			var messageInput = document.getElementById('messageInput'),
+			msg = messageInput.value;
+			messageInput.value = '';
+			messageInput.focus();
+			if (msg.trim().length!=0) {
+				that.socket.emit('postMsg',msg); //把消息显示在服务器中
+				that._displayNewMsg('me',msg); //把自己的消息显示在自己的窗口中
+			};
+		},false);
+
+		
+	},
+	_displayNewMsg:function(user,msg,color){
+		var container = document.getElementById('historyMsg'),
+		msgToDisplay = document.createElement('p');
+		date = new Date().toTimeString().substr(0,8);
+		msgToDisplay.style.color = color || '#000';
+		msgToDisplay.innerHTML = user +   date + msg; 
+		container.appendChild(msgToDisplay);
+		container.scrollTop = container.scrollHeight;
 	}
 };
 
